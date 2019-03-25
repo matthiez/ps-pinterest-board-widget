1fb78ef5b4cb70f553e7ad70666a61689211082c
Andre Matthies <andre@ecolos.de>
2019-03-25 12:07:33 +0100

    added 1.7 support

---
+++ README.md
@@ -0,0 +1,16 @@
+# PrestaShop 1.6/1.7 Pinterest Board Widget Module
+
+This module lets you add a Pinterest board widget to your hook of choice.
+Useful if one pin isn’t enough, add an entire board (up to 50 pins) to your site.
+
+# Options
+
+**Enable Board Widget?** This option defines whether the module is enabled or not.
+
+**Board Profile URL** The board profile URL, e.g. ``https://www.pinterest.com/anapinskywalker/style/``.
+
+**Board width inside the widget** The board widgets width. Does not include the white border on each side.
+
+**Board height inside the widget** The board widgets height. Does not include the white borders above and below.
+
+**Width of the Pin thumbnails within the widget** Defines how wide a single pin must be.
--- backendhelperform.php
+++ backendhelperform.php
@@ -1,124 +1,144 @@
-<?php if (!defined('_PS_VERSION_')) exit;
-
+<?php
 /**
- * Class BackendHelperForm
+ * NOTICE OF LICENSE
+ *
+ * This file is licenced under the Software License Agreement.
+ * With the purchase or the installation of the software in your application
+ * you accept the licence agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ * @author    André Matthies
+ * @copyright 2018-present André Matthies
+ * @license   LICENSE.md
  */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
 class BackendHelperForm extends HelperForm
 {
-    /**
-     * BackendHelperForm constructor.
-     * @param $name
-     */
-    public function __construct($name) {
+    public function __construct(string $name)
+    {
         parent::__construct();
 
         $default_lang = Configuration::get('PS_LANG_DEFAULT');
 
         $this->module = $this;
 
+        $this->name = $name;
+
         $this->name_controller = $name;
 
         $this->token = Tools::getAdminTokenLite('AdminModules');
 
-        $this->currentIndex = AdminController::$currentIndex . '&configure=' . $name;
+        $this->currentIndex = AdminController::$currentIndex . "&configure=$name";
 
         $this->default_form_language = $default_lang;
 
         $this->allow_employee_form_lang = $default_lang;
 
-        $this->title = $this->displayName;
+        $this->title = $this->name;
 
         $this->show_toolbar = true;
 
         $this->toolbar_scroll = true;
 
-        $this->submit_action = 'submit' . $name;
+        $this->submit_action = "submit$name";
 
-        $this->toolbar_btn = [
+        $this->toolbar_btn = array(
             'save' =>
-                [
+                array(
                     'desc' => $this->l('Save'),
-                    'href' => AdminController::$currentIndex . '&configure=' . $name . '&save' . $name .
-                        '&token=' . Tools::getAdminTokenLite('AdminModules'),
-                ],
-            'back' => [
+                    'href' => AdminController::$currentIndex
+                        . "&configure=$name&save$name&token="
+                        . Tools::getAdminTokenLite('AdminModules'),
+                ),
+            'back' => array(
                 'href' => AdminController::$currentIndex . '&token=' . Tools::getAdminTokenLite('AdminModules'),
                 'desc' => $this->l('Back to list')
-            ]
-        ];
+            )
+        );
 
-        $this->fields_value = [
-            'config[PINTEREST_BOARD_WIDGET]' => Configuration::get('PINTEREST_BOARD_WIDGET'),
-            'config[PINTEREST_BOARD_WIDGET_URL]' => Configuration::get('PINTEREST_BOARD_WIDGET_URL'),
-            'config[PINTEREST_BOARD_WIDGET_BOARD_WIDTH]' => Configuration::get('PINTEREST_BOARD_WIDGET_BOARD_WIDTH'),
-            'config[PINTEREST_BOARD_WIDGET_SCALE_HEIGHT]' => Configuration::get('PINTEREST_BOARD_WIDGET_SCALE_HEIGHT'),
-            'config[PINTEREST_BOARD_WIDGET_SCALE_WIDTH]' => Configuration::get('PINTEREST_BOARD_WIDGET_SCALE_WIDTH')
-        ];
+        $this->fields_value = array(
+            'config[EOO_PINTEREST_BOARD_WIDGET]' =>
+                Configuration::get('EOO_PINTEREST_BOARD_WIDGET'),
+            'config[EOO_PINTEREST_BOARD_WIDGET_URL]' =>
+                Configuration::get('EOO_PINTEREST_BOARD_WIDGET_URL'),
+            'config[EOO_PINTEREST_BOARD_WIDGET_BOARD_WIDTH]' =>
+                Configuration::get('EOO_PINTEREST_BOARD_WIDGET_BOARD_WIDTH'),
+            'config[EOO_PINTEREST_BOARD_WIDGET_SCALE_HEIGHT]' =>
+                Configuration::get('EOO_PINTEREST_BOARD_WIDGET_SCALE_HEIGHT'),
+            'config[EOO_PINTEREST_BOARD_WIDGET_SCALE_WIDTH]' =>
+                Configuration::get('EOO_PINTEREST_BOARD_WIDGET_SCALE_WIDTH')
+        );
 
-        $this->fields_form = [['form' => [
-            'legend' => [
+        $this->fields_form = array(array('form' => array(
+            'legend' => array(
                 'title' => $this->l('Settings'),
                 'icon' => 'icon-cogs'
-            ],
-            'input' => [
-                [
+            ),
+            'input' => array(
+                array(
                     'type' => 'switch',
-                    'name' => 'config[PINTEREST_BOARD_WIDGET]',
+                    'name' => 'config[EOO_PINTEREST_BOARD_WIDGET]',
                     'label' => $this->l('Enable Board Widget?'),
-                    'hint' => $this->l('When one Pin isn’t enough, add an entire board (up to 50 Pins) to your site.'),
+                    'hint' =>
+                        $this->l('When one Pin isn’t enough, add an entire board (up to 50 Pins) to your site.'),
                     'is_bool' => true,
                     'required' => false,
-                    'values' => [
-                        [
+                    'values' => array(
+                        array(
                             'id' => 'board_widget_on',
                             'value' => 1,
                             'label' => $this->l('Yes')
-                        ],
-                        [
+                        ),
+                        array(
                             'id' => 'board_widget_off',
                             'value' => 0,
                             'label' => $this->l('No')
-                        ]
-                    ]
-                ],
-                [
+                        )
+                    )
+                ),
+                array(
                     'type' => 'text',
-                    'name' => 'config[PINTEREST_BOARD_WIDGET_URL]',
+                    'name' => 'config[EOO_PINTEREST_BOARD_WIDGET_URL]',
                     'label' => $this->l('Board Profile URL'),
-                    'hint' => 'e.g. https://www.pinterest.com/pinterest/official-news/',
+                    'hint' => 'e.g. https://www.pinterest.com/anapinskywalker/style/',
                     'required' => true
-                ],
-                [
+                ),
+                array(
                     'type' => 'text',
-                    'name' => 'config[PINTEREST_BOARD_WIDGET_BOARD_WIDTH]',
+                    'name' => 'config[EOO_PINTEREST_BOARD_WIDGET_BOARD_WIDTH]',
                     'label' => $this->l('Board width inside the widget.'),
                     'hint' => $this->l('This width does not include the white border on either side.'),
                     'desc' => 'Minimum width of 130px. Default: Fill width of parent.',
                     'suffix' => 'px',
                     'required' => false
-                ],
-                [
+                ),
+                array(
                     'type' => 'text',
-                    'name' => 'config[PINTEREST_BOARD_WIDGET_SCALE_HEIGHT]',
+                    'name' => 'config[EOO_PINTEREST_BOARD_WIDGET_SCALE_HEIGHT]',
                     'label' => $this->l('Board height inside the widget.'),
                     'hint' => $this->l('This does not include the white border above and below.'),
                     'desc' => 'Minimum height of 60px. Default: 175px.',
                     'suffix' => 'px',
                     'required' => false
-                ],
-                [
+                ),
+                array(
                     'type' => 'text',
-                    'name' => 'config[PINTEREST_BOARD_WIDGET_SCALE_WIDTH]',
+                    'name' => 'config[EOO_PINTEREST_BOARD_WIDGET_SCALE_WIDTH]',
                     'label' => $this->l('Width of the Pin thumbnails within the widget.'),
                     'desc' => 'Minimum width of 60px. Default: 92px',
                     'suffix' => 'px',
                     'required' => false
-                ],
-            ],
-            'submit' => [
+                ),
+            ),
+            'submit' => array(
                 'title' => $this->l('Save'),
                 'class' => 'btn btn-default pull-right'
-            ]
-        ]]];
+            )
+        )));
     }
 }
